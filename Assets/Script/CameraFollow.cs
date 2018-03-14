363491dac8a54b260ea8c25db21bec27de171a40
Michael Dessureault <dessureaultmichael@gmail.com>
2018-03-13 20:09:46 -0400

    Camera Follow
    Also has a temp image for player and ground

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private Transform trackObj;
+    [SerializeField] private float xOffset;
+
+    private float originalY;
+    private float originalZ;
+
+    private void Start() {
+        originalY = transform.position.y;
+        originalZ = transform.position.z;
+    }
+
+    void LateUpdate () {
+        transform.position = new Vector3(trackObj.transform.position.x + xOffset, originalY, originalZ);
+	}
+}
